bf0c8cd7a4a598328af8637f55756b9b0b449008
fedo-code <polash.gohain14@gmail.com>
2025-08-30 21:53:48 +0530

    DLt readme

--- README.md
+++
@@ -1,2 +0,0 @@
-# restaurant-order-system
-A pure JavaScript SPA site where we can browse and order the required item, making the payment online
--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.use((req, res) => {
 
 
 // Server Start
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
